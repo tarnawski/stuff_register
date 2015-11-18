688315b3e2e4c19f52ab08964df13da85b3dc861
Tomasz Tarnawski <tarnawski27@gmail.com>
2015-11-18 19:25:41 +0100

    Test

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20151106160421) do
   create_table "models", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.stra   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",          default: 0,  null: false
