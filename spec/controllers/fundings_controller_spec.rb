6b09ff6ece0d3c5209ec5bdd0878d895f3cbb956
Tomasz Tarnawski <tarnawski27@gmail.com>
2015-11-19 00:28:56 +0100

    Test

--- spec/controllers/fundings_controller_spec.rb
+++ spec/controllers/fundings_controller_spec.rb
@@ -19,7 +19,7 @@ require 'rails_helper'
 # that an instance is receiving a specific message.
 
 RSpec.describe FundingsController, :type => :controller do
-
+ 
   # This should return the minimal set of attributes required to create a valid
   # Funding. As you add validations to Funding, be sure to
   # adjust the attributes here as well.
